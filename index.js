658b0eca5ad895ee18f62a44e6b5518ceca38870
ahmednurmok <ahmednurmok@gmail.com>
2025-11-08 19:59:41 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+
+//Task: Change button text colour to yellow and background to red
+
+document.querySelector("button").style.Color = 'yellow';
+document.querySelector("button").style.backgroundColor = 'red';
